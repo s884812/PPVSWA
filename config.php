2643f899be870b27e50117f3f474020669f263f9
Legend <s884812@gmail.com>
2013-05-05 12:43:47 +0800

    PPVSWA
    Config file edited.

--- config.php
+++ config.php
@@ -1,20 +1,21 @@
 <?php
 $dbserver = "localhost"; //your MySQL server address
-$dbuser = "root"; //your MySQL user name
-$dbpassword = "asdf5566isme"; //your MySQL user password
-$dbname = "vpn"; //your MySQL database name for your site
-$sitename = "puvpn"; //your site's name
-$vpnaddress = "jp2.puvpn.com"; //your pptp vpn server's address
+$dbuser = ""; //your MySQL user name
+$dbpassword = ""; //your MySQL user password
+$dbname = ""; //your MySQL database name for your site
+$sitename = ""; //your site's name
+$vpnaddress = ""; //your pptp vpn server's address
 $vpnname = "pptpd"; //your pptp vpn server's name
-$sitepassword = "asdf5566isme"; //set a password for this web admin program
-$siteurl = "http://puvpn.net"; //your site's url with http://
-$siteemail = "s884812@gmail.com"; //your site's admin email address
+$sitepassword = ""; //set a password for this web admin program
+$siteurl = ""; //your site's url with http://
+$siteemail = ""; //your site's admin email address
 $logfile = "/var/log/messages"; //your 'messages' log file
 $sublogfile = "sublog"; //sublog file created by PPVSWA, Need Write permission for that.
 $analyzedlogfile = "analyzedlog"; //analyzed system log file. Do Not Delete Or Modify.
-$maxip = "172.16.36.1";//client MAX ipv4 . The last 8 bit SHOULD BE 254. And you should alow this network access NAT by setting iptables.
+$maxip = "192.168.1.254";//client MAX ipv4 . The last 8 bit SHOULD BE 254. And you should alow this network access NAT by setting iptables.
 $secretfile = "/etc/ppp/chap-secrets"; //your chap-secrets file location
 $oldsecretfile = "/etc/ppp/chap-secrets.old"; //your old chap-secrets file location
 
+//注意！此处所有变量都必须填全，否则程序无法运行
 //ALL varables in this file MUST be set.
 ?>
