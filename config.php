132f51b34d15ddfaa389a788858135ecad900b16
Legend <s884812@gmail.com>
2013-05-05 12:46:38 +0800

    PPVSWA
    confgi file edited

--- config.php
+++ config.php
@@ -16,6 +16,5 @@ $maxip = "192.168.1.254";//client MAX ipv4 . The last 8 bit SHOULD BE 254. And y
 $secretfile = "/etc/ppp/chap-secrets"; //your chap-secrets file location
 $oldsecretfile = "/etc/ppp/chap-secrets.old"; //your old chap-secrets file location
 
-//注意！此处所有变量都必须填全，否则程序无法运行
 //ALL varables in this file MUST be set.
 ?>
